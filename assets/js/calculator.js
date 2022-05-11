578b3b2221dbebddd7792db964a232c978253d81
philswatton <philswatton@gmail.com>
2022-05-11 17:27:52 +0100

    tokenise the formula

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -22,39 +22,29 @@ function calc_limit() {
     }
 }
 
-// Step 2: Validate and parse input, show error if invalid
+// Step 2: Validate, tokenise, and parse input, show error if invalid
 
 // Validation
 // 1) Numbers can be as long as user wants
 // 2) treat RANGES as numbers
 // 3) But can't repeat /-+*.~ more than once in a row
-
-// Parse:
-// 1) establish constants vs normal distributions
-// 2) preserve operator placements
-
-// Some temporary examples to work with
-const f1 = "2+2~5-3";
-const f2 = "25.6*2~4/3";
-const f3 = "30*4~10.5+10+10~20";
-// const f4 = "20++30";
-// const f5 = "20~~~30";
-
-const reValid = /^\-{2,}|\+{2,}|\-{3,}|\*{2,}|\/{2,}|~{2,}|\.{2,}/;
+// 4) Can't start with operators
+const reValid = /^\+|^\*|^\/|^\-{2,}|\+{2,}|\-{3,}|\*{2,}|\/{2,}|~{2,}|\.{2,}/;
 function calc_validate(f) {
     test = reValid.test(f)
     if (test) {
         console.log("Formula error");
     } else {
-        tokenise(f);
+        const tokens = tokenise(f);
+        console.log(tokens)
     }
 }
 
+// Tokenise
 function isDigit(char) {return(/\d+\.{0,1}\d*/.test(char));}
-
 function tokenise(f) {
     // Output array
-    let out=[];
+    let tokens=[];
 
     // Temp log
     console.log(f);
@@ -68,28 +58,31 @@ function tokenise(f) {
     for (let i = 0; i < chars.length; i++) {
         test = isDigit(chars[i])
         if (test) {
-            if (out.length == j) {
-                out.push(chars[i]);
+            if (tokens.length == j) {
+                tokens.push(chars[i]);
             } else {
-                out[j] = out[j] + chars[i];
+                tokens[j] = tokens[j] + chars[i];
             }
         } else if (/\./.test(chars[i]) | /~/.test(chars[i])) {
-            out[j] = out[j] + chars[i];
+            tokens[j] = tokens[j] + chars[i];
         } else if ((i == 0 | /\+|\-|\*|\//.test(chars[i-1])) & chars[i] == "-") {
-            out[j] = chars[i];
+            tokens[j] = chars[i];
         // } else if (/\-/.test(chars[i])) {
         //     if (chars[i-1] == "-" & chars[i])
         } else {
-            out[j+1] = chars[i];
+            tokens[j+1] = chars[i];
             j+=2;
         }
     }
 
-    console.log(out);
+    return(tokens);
 
 }
 
-function parse(f) {
+// Parse:
+// 1) establish constants vs normal distributions
+// 2) preserve operator placements
+function parse(tokens) {
 
     // Split input to 
 
@@ -99,27 +92,6 @@ function parse(f) {
 
 
 }
-
-// function calc_parse(f) {
-
-
-
-//     // const outQueue=[];
-//     // const opStack=[];
-//     const step1 = f.split("-");
-//     //const step2 = step1.split("+");
-
-//     console.log(step1);
-//     console.log(step2);
-// }
-
-// calc_validate(f1);
-// calc_validate(f2);
-// calc_validate(f3);
-// calc_validate(f4);
-// calc_validate(f5);
-
-// calc_parse(f1);
 
 // Step 3: Run monte carlo simulation
 
